88c852205e4595a6b12202e661b73dae2f940b8a
Nitchanan-K <teelajen111@gmail.com>
2022-08-26 17:05:41 +0700

    update 26-aug-2 plot with no name(need to add name at plot

---
+++ functions/plot_with_bokeh.py
@@ -0,0 +1,81 @@
+import talib
+from yahoo_fin.stock_info import *
+from yahoo_fin.stock_info import get_analysts_info
+import yahoo_fin.stock_info as si
+import pandas as pd
+import os
+from math import pi
+from math import pi
+from bokeh.plotting import figure
+from bokeh.io import output_notebook,show
+from bokeh.resources import INLINE
+
+# make dataframe
+df_main = pd.DataFrame()
+datafiles = os.listdir('C:/Users/Admin pc/PycharmProjects/steamlit-webapp/dataset/daily/')
+
+# test dt
+df_new = pd.DataFrame()
+
+for filesname in datafiles:
+    df = pd.read_csv(f'C:/Users/Admin pc/PycharmProjects/steamlit-webapp/dataset/daily/{filesname}')
+    df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
+    df_main = pd.concat([df_main,df.tail(10)])
+    ee = 0
+    result = []
+    for n in df_main['ticker']:
+        ee += 1
+        result.append(ee)
+        if ee == 10:
+            ee = 0
+    df_main['Result'] = result
+    df_main['New_col'] = df_main['ticker'] + "-" + df_main['Result'].astype(str)
+    df_main.set_index('New_col', inplace=True)
+
+def make_data_for_plot(crypto_name):
+    Open = [df_main.loc[f'{crypto_name}-1']['open'],df_main.loc[f'{crypto_name}-2']['open'],df_main.loc[f'{crypto_name}-3']['open'],df_main.loc[f'{crypto_name}-4']['open'],df_main.loc[f'{crypto_name}-5']['open'],df_main.loc[f'{crypto_name}-6']['open'],df_main.loc[f'{crypto_name}-7']['open'],df_main.loc[f'{crypto_name}-8']['open'],df_main.loc[f'{crypto_name}-9']['open'],df_main.loc[f'{crypto_name}-10']['open']]
+    close = [df_main.loc[f'{crypto_name}-1']['close'],df_main.loc[f'{crypto_name}-2']['close'],df_main.loc[f'{crypto_name}-3']['close'],df_main.loc[f'{crypto_name}-4']['close'],df_main.loc[f'{crypto_name}-5']['close'],df_main.loc[f'{crypto_name}-6']['close'],df_main.loc[f'{crypto_name}-7']['close'],df_main.loc[f'{crypto_name}-8']['close'],df_main.loc[f'{crypto_name}-9']['close'],df_main.loc[f'{crypto_name}-10']['close']]
+    low = [df_main.loc[f'{crypto_name}-1']['low'],df_main.loc[f'{crypto_name}-2']['low'],df_main.loc[f'{crypto_name}-3']['low'],df_main.loc[f'{crypto_name}-4']['low'],df_main.loc[f'{crypto_name}-5']['low'],df_main.loc[f'{crypto_name}-6']['low'],df_main.loc[f'{crypto_name}-7']['low'],df_main.loc[f'{crypto_name}-8']['low'],df_main.loc[f'{crypto_name}-9']['low'],df_main.loc[f'{crypto_name}-10']['low']]
+    high = [df_main.loc[f'{crypto_name}-1']['high'],df_main.loc[f'{crypto_name}-2']['high'],df_main.loc[f'{crypto_name}-3']['high'],df_main.loc[f'{crypto_name}-4']['high'],df_main.loc[f'{crypto_name}-5']['high'],df_main.loc[f'{crypto_name}-6']['high'],df_main.loc[f'{crypto_name}-7']['high'],df_main.loc[f'{crypto_name}-8']['high'],df_main.loc[f'{crypto_name}-9']['high'],df_main.loc[f'{crypto_name}-10']['high']]
+    date = [df_main.loc[f'{crypto_name}-1']['date'],df_main.loc[f'{crypto_name}-2']['date'],df_main.loc[f'{crypto_name}-3']['date'],df_main.loc[f'{crypto_name}-4']['date'],df_main.loc[f'{crypto_name}-5']['date'],df_main.loc[f'{crypto_name}-6']['date'],df_main.loc[f'{crypto_name}-7']['date'],df_main.loc[f'{crypto_name}-8']['date'],df_main.loc[f'{crypto_name}-9']['date'],df_main.loc[f'{crypto_name}-10']['date']]
+    data_frame = pd.DataFrame({'open':Open,'close':close,'low':low,'high':high,'date':date})
+    data_frame['date2'] = pd.to_datetime(data_frame['date'],errors='coerce')
+
+    #print(data_frame)
+    return data_frame
+
+def plot_candle_stick(crypto_name):
+    data_frame = make_data_for_plot(crypto_name)
+    inc = data_frame.close > data_frame.open
+    dec = data_frame.open > data_frame.close
+    w = 12*60*60*1000 # half day in ms
+
+    TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+    p = figure(x_axis_type="datetime", tools=TOOLS,title="test")
+    p.xaxis.major_label_orientation = pi/4
+    p.grid.grid_line_alpha=0.8
+
+    p.segment (data_frame['date2'],data_frame['high'], data_frame['date2'],data_frame['low'],color = 'black')
+    p.vbar(data_frame['date'][inc], w, data_frame['open'][inc], data_frame['close'][inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(data_frame.date2[inc], w, data_frame.open[inc], data_frame.close[inc], fill_color="#30d9a0", line_color="black")
+    p.vbar(data_frame.date2[dec], w, data_frame.open[dec], data_frame.close[dec], fill_color="#F2583E", line_color="black")
+
+    #show(p)
+    return p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_webapp.py
+++ test_webapp.py
@@ -6,7 +6,27 @@ import pandas as pd
 import numpy as np
 from bokeh.plotting import figure
 from PIL import Image
+from patterns_list import candlestick_patterns
+import talib
+import csv
 
+# make dataframe
+df_main = pd.DataFrame()
+datafiles = os.listdir('dataset/daily')
+for filesname in datafiles:
+    df = pd.read_csv(f'dataset/daily/{filesname}')
+    df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
+    df_main = pd.concat([df_main,df.tail(5)])
+    ee = 0
+    result = []
+    for n in df_main['ticker']:
+        ee += 1
+        result.append(ee)
+        if ee == 5:
+            ee = 0
+    df_main['Result'] = result
+    df_main['New_col'] = df_main['ticker'] + "-" + df_main['Result'].astype(str)
+    df_main.set_index('New_col', inplace=True)
 
 # functions
 
@@ -15,6 +35,11 @@ def load_data(data_name):
     data = qs.utils.download_returns(f'{data_name}')
     return data
 
+def load_data_pattern(data_name):
+    for i in range(1,6):
+        print(df_main.loc[f'{data_name}-{i}'])
+        data = df_main.loc[f'{data_name}-{i}']
+        return data
 
 def plot_with_quant(data_name,plot_name):
     data_dt = qs.utils.download_returns(f'{data_name}')
@@ -36,7 +61,8 @@ def plot_benchmark_report(data_name):
 # set title
 st.title('Test WebAPP With QuantStats')
 
-
+# Dict
+crypto_dict = {}
 
 
 # expander Plot Data
@@ -112,9 +138,74 @@ with st.expander("Show Return %"):
         st.write(load_data(select_crypto_name))
 
 
+from candlestick_pattern import candlestick_patterns
+
+# expander scan candle
+with st.expander("Patterns Scaner"):
+
+    # Select Crypto to find pattern button
+    select_crypto_name_scan = st.selectbox('Select Crypto to find pattern', crypto_names.crypto_name_tuple)
+
+    # Select Patterns button
+    patterns = st.selectbox('Select Patterns',candlestick_patterns)
+    print(candlestick_patterns.get(patterns))
+
+    # dict
+    if st.button(f'Scan for {patterns}'):
+        datafiles = os.listdir('dataset/daily')  # get lists of all CSV files
+
+        #load_data_pattern(select_crypto_name_scan)
+
+        import functions.plot_with_bokeh
+        #functions.plot_with_bokeh.plot_candle_stick(select_crypto_name_scan)
+        #st.bokeh_chart(functions.plot_with_bokeh.plot_candle_stick(select_crypto_name_scan), use_container_width=True)
+
+
+        for filename in datafiles:  # loop for each file
+            df = pd.read_csv(f'dataset/daily/{filename}')
+            df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
+
+
+            pattern_function = getattr(talib, candlestick_patterns.get(patterns))
+               # getattr talib.pattern / pattern = select option input from web
+            symbol = filename.split('.')[0]  # get symbol from filename
+
+            with open('dataset/crypto_list.csv') as f:
+                for row in csv.reader(f):
+                    crypto_dict[row[0]] = {'crypto': row[0].split('-')[0] + "-USD"}
+
+            try:
+
+                # result is the daily that returned after applied ta-lib candle pattern
+                result = pattern_function(df['open'], df['high'], df['low'], df['close'])
+
+
+                # last is the result lasttest value
+                last = result.tail(1).values[0]
+
+
+                if last == 100:
+                    crypto_dict[symbol][candlestick_patterns.get(patterns)] = 'bullish'  # set pattern value to use
+                    print(crypto_dict[symbol], "\n")
+                    print(crypto_dict[symbol]['crypto'])
+                    functions.plot_with_bokeh.plot_candle_stick(crypto_dict[symbol]['crypto'])
+                    st.bokeh_chart(functions.plot_with_bokeh.plot_candle_stick(crypto_dict[symbol]['crypto']))
+
+
+                elif last == -100:
+                    crypto_dict[symbol][candlestick_patterns.get(patterns)] = 'bearish'
+                    print(crypto_dict[symbol], "\n")
+                    functions.plot_with_bokeh.plot_candle_stick(crypto_dict[symbol]['crypto'])
+                    st.bokeh_chart(functions.plot_with_bokeh.plot_candle_stick(crypto_dict[symbol]['crypto']))
 
 
 
+                else:
+                    crypto_dict[symbol][patterns] = None
+
+
+            except:
+                pass
 
 
 
