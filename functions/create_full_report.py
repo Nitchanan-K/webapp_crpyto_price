7246d3d479ee2b415a3b77025fd5df48e3b33c5e
Nitchanan <nitchanan_katinasatian@hotmail.com>
2023-03-01 12:00:39 +0700

    try qs fullreport not html report

--- functions/create_full_report.py
+++ functions/create_full_report.py
@@ -2,4 +2,4 @@ import quantstats as qs
 
 def create_report(dataname,date_dt):
     print(dataname,date_dt)
-    qs.reports.html(date_dt, title=f'{dataname} Report Investments', output='output/BTC_report.html', mode='full')
+    qs.reports.full(date_dt)
