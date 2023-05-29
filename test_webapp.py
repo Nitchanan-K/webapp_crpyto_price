1d985fb0985b28c48ba84894b00b0d19cce3a2ef
nitchanan.k <nitchanan_katinasatian@hotmail.com>
2023-05-29 09:30:13 +0700

    Wait for debuging full report2

--- test_webapp.py
+++ test_webapp.py
@@ -92,37 +92,37 @@ with st.expander("Plot Data"):
                 mime="image/png"
             )
 
-with st.expander("Full Report"):
+# with st.expander("Full Report"):
 
-    # Default Report
-    default_title = '<p style="font-family:Helvetica; color:Black; font-size: 25px;">Default Report</p>'
-    st.markdown(default_title, unsafe_allow_html=True)
+#     # Default Report
+#     default_title = '<p style="font-family:Helvetica; color:Black; font-size: 25px;">Default Report</p>'
+#     st.markdown(default_title, unsafe_allow_html=True)
 
-    # select box for crypto name
-    select_crypto_name_report = st.selectbox('Select Crypto For analyze', crypto_names.crypto_name_tuple
-                                      )
-    # sub header
-    st.subheader(f'{select_crypto_name_report}')
-    # plot default report
-    if st.button('Create Full Report'):
-        plot_full_report(select_crypto_name_report)
-        tearsheet = open("quantstats-tearsheet.html")
-        components.html(tearsheet.read(),height=1000,width=700,scrolling=True)
+#     # select box for crypto name
+#     select_crypto_name_report = st.selectbox('Select Crypto For analyze', crypto_names.crypto_name_tuple
+#                                       )
+#     # sub header
+#     st.subheader(f'{select_crypto_name_report}')
+#     # plot default report
+#     if st.button('Create Full Report'):
+#         plot_full_report(select_crypto_name_report)
+#         tearsheet = open("quantstats-tearsheet.html")
+#         components.html(tearsheet.read(),height=1000,width=700,scrolling=True)
 
-    # Benchmark Report
-    original_title = '<p style="font-family:Helvetica; color:Black; font-size: 25px;">Benchmark Report</p>'
-    st.markdown(original_title, unsafe_allow_html=True)
+#     # Benchmark Report
+#     original_title = '<p style="font-family:Helvetica; color:Black; font-size: 25px;">Benchmark Report</p>'
+#     st.markdown(original_title, unsafe_allow_html=True)
 
-    # select benchmark box
-    select_benchmark_crypto = st.selectbox('VS Benchmark',crypto_names.crypto_name_tuple
-                                   )
-    # sub header
-    st.subheader(f'{select_crypto_name_report} | vs | {select_benchmark_crypto}')
+#     # select benchmark box
+#     select_benchmark_crypto = st.selectbox('VS Benchmark',crypto_names.crypto_name_tuple
+#                                    )
+#     # sub header
+#     st.subheader(f'{select_crypto_name_report} | vs | {select_benchmark_crypto}')
 
-    # plot benchmark report
-    if st.button('Create Benchmark Report'):
-        plot_benchmark_report(select_crypto_name_report,select_benchmark_crypto)
-        tearsheet_benchmark = open("quantstats-tearsheet.html")
-        components.html(tearsheet_benchmark.read(), height=1000, width=700, scrolling=True)
+#     # plot benchmark report
+#     if st.button('Create Benchmark Report'):
+#         plot_benchmark_report(select_crypto_name_report,select_benchmark_crypto)
+#         tearsheet_benchmark = open("quantstats-tearsheet.html")
+#         components.html(tearsheet_benchmark.read(), height=1000, width=700, scrolling=True)
 
 
